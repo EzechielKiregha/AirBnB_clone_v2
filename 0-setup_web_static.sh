5aa185318eff87adfca756c3dcb8d1f7907cae2a
EzechielKiregha <ekiregha@gmail.com>
2024-05-05 17:58:56 +0200

    setting up my web static pages

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,58 +7,39 @@ if ! command -v nginx &> /dev/null; then
     sudo apt-get install -y nginx
 fi
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-
-# Create a fake HTML file for testing
-echo "<html>
-    <head>
-    </head>
-    <body>
-        Holberton School
-    </body>
-</html>
-" | sudo tee /data/web_static/releases/test/index.html
-
-# Create a symbolic link
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
 
-# Remove the default Nginx configuration
-sudo rm -f /etc/nginx/sites-enabled/custom_conf
-
-# Update Nginx configuration
-sudo tee /etc/nginx/sites-available/custom_config <<EOF
-server {
-    listen 80;
-    listen [::]:80;
-    server_name _;
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
     location /hbnb_static {
-        alias /data/web_static/current/;
+	alias /data/web_static/current;
+	index index.html index.htm;
+    }
+
+    location /redirect_me {
+	return 301 http://cuberule.com/;
     }
 
     error_page 404 /404.html;
-
-    location /404.html {
-        root /var/www/html;
-        internal;
+    location /404 {
+      root /var/www/html;
+      internal;
     }
+}" > /etc/nginx/sites-available/default
 
-    location / {
-        root /var/www/html;
-        index index.html index.htm;
-    }
-}
-EOF
-
-# Enable the new Nginx site configuration
-sudo ln -sf /etc/nginx/sites-available/custom_config /etc/nginx/sites-enabled/
-
-# Restart Nginx
-sudo systemctl restart nginx
-
-exit 0
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+# test nginx 
+sudo nginx -t
+# restart nginx
+sudo service nginx restart
